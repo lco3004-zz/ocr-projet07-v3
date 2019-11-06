68319e6789400593fca21a5a76712d0a759a7bd7
cordier <laurent.cordier3004@gmail.com>
2019-11-06 16:39:24 +0100

    BatchMail avec HttpClient . preparation send email (wip)

--- service_batchmail/src/main/java/fr/ocr/service_batchmail/ServiceBatchmailApplication.java
+++ service_batchmail/src/main/java/fr/ocr/service_batchmail/ServiceBatchmailApplication.java
@@ -5,9 +5,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import javax.mail.MessagingException;
+import java.io.IOException;
+
 @SpringBootApplication
 @EnableScheduling
-public class ServiceBatchmailApplication {
+public class ServiceBatchmailApplication  implements  CommandLineRunner{
 
     public static void main(String[] args) {
 
@@ -16,4 +19,8 @@ public class ServiceBatchmailApplication {
     }
 
 
+    @Override
+    public void run(String... args) throws IOException, MessagingException {
+        sendEmail();
+    }
 }
