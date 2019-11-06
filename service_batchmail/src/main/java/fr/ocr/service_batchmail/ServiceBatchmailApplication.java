1b44e09165855251a7b166be7f6148c70295341c
cordier <laurent.cordier3004@gmail.com>
2019-11-06 17:36:06 +0100

    BatchMail avec HttpClient . preparation send email SUITE (wip)

--- service_batchmail/src/main/java/fr/ocr/service_batchmail/ServiceBatchmailApplication.java
+++ service_batchmail/src/main/java/fr/ocr/service_batchmail/ServiceBatchmailApplication.java
@@ -1,16 +1,27 @@
 package fr.ocr.service_batchmail;
 
+import fr.ocr.service_batchmail.task.ScheduledMailer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 import javax.mail.MessagingException;
 import java.io.IOException;
+import java.util.Arrays;
 
 @SpringBootApplication
 @EnableScheduling
 public class ServiceBatchmailApplication  implements  CommandLineRunner{
+
+    @Autowired
+    private ScheduledMailer scheduledMailer;
+
+    private static final Logger log = LoggerFactory.getLogger(ServiceBatchmailApplication.class);
 
     public static void main(String[] args) {
 
@@ -18,9 +29,14 @@ public class ServiceBatchmailApplication  implements  CommandLineRunner{
 
     }
 
-
     @Override
-    public void run(String... args) throws IOException, MessagingException {
-        sendEmail();
+    public void run(String... args)  {
+        if (args.length >0)
+            try {
+                scheduledMailer.emailingToOverDueBorrowers();
+
+            }catch (RuntimeException e) {
+                log.error("Exception levee suite appel de scheduledMailer.emailingToOverDueBorrowers()" + Arrays.toString(e.getStackTrace()));
+            }
     }
 }
--- service_batchmail/src/main/java/fr/ocr/service_batchmail/task/ScheduledMailer.java
+++ service_batchmail/src/main/java/fr/ocr/service_batchmail/task/ScheduledMailer.java
@@ -4,10 +4,14 @@ import fr.ocr.service_batchmail.domain.InfosMailDtoBatch;
 import fr.ocr.service_batchmail.service.BatchEmailService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
+import javax.mail.MessagingException;
+import java.io.IOException;
+import java.util.Arrays;
 
 
 @Component
@@ -17,18 +21,41 @@ public class ScheduledMailer {
 
     final BatchEmailService batchEmailService;
 
-    public ScheduledMailer(BatchEmailService batchEmailService) {
+    private final JavaMailSender javaMailSender;
+
+    public ScheduledMailer(BatchEmailService batchEmailService, JavaMailSender javaMailSender) {
         this.batchEmailService = batchEmailService;
+        this.javaMailSender = javaMailSender;
     }
 
     @Scheduled(cron = "0 0 10 * * *")
-    public void emailingToOverDueBorrowers() {
-        batchEmailService.getPretHorsDelai().forEach( x-> {
-            sendEmail(x.getCourriel());
+    public void emailingToOverDueBorrowers() throws  RuntimeException{
+        batchEmailService.getPretHorsDelai().forEach(infosMailDtoBatch -> {
+            try {
+                sendEmail(infosMailDtoBatch);
+            } catch (IOException e) {
+                log.error("IOException levee suite appel de ScheduledMailer.sendEmail" + Arrays.toString(e.getStackTrace()));
+                throw  new RuntimeException(e.getLocalizedMessage());
+            } catch (MessagingException e) {
+                log.error("MessagingException levee suite appel de ScheduledMailer.sendEmail" + Arrays.toString(e.getStackTrace()));
+                throw  new RuntimeException(e.getLocalizedMessage());
+            }
         });
-
     }
-    void sendEmail(String email) {
+    void sendEmail(InfosMailDtoBatch infosMailDtoBatch) throws IOException, MessagingException {
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
 
+        mailMessage.setTo(infosMailDtoBatch.getCourriel());
+        mailMessage.setSubject("Relance Usager :" + infosMailDtoBatch.getNom());
+        mailMessage.setText("Bonjour, vous avez emprunté l'ouvrage :"+
+                 "\n "+infosMailDtoBatch.getTitre() +
+                 "\n De "+infosMailDtoBatch.getAuteur()+
+                 "\n Le  "+infosMailDtoBatch.getDateEmprunt() +
+                 "\n \n \n Le délai de 4 semaines est dépassé." +
+                 "\n Merci de nous retourner cet ouvrage "+
+                 "\n L'équipe Municipale ");
+
+        javaMailSender.send(mailMessage);
     }
+
 }
